80e59a8652a1c3d3e6263bac47792768879683c0
M4S73RMIND <30705963+M4S73RMIND@users.noreply.github.com>
2017-08-04 03:33:51 +0200

    Add files via upload

--- ExtendedDebuffs.lua
+++ ExtendedDebuffs.lua
@@ -1,9 +1,15 @@
+do
+	local ICON_SIZE = options.iconSize
+end
+
 function
-	sp(f,i) tr="TOPRIGHT";
-	f2=f.debuffFrames;
-	s=f2[1]:GetWidth();
-	f3=f2[i];f3:SetSize(s,s);
-	f3:ClearAllPoints();
+	sp(f,i)
+	tr="TOPRIGHT"
+	f2=f.debuffFrames
+	s=f2[1]:GetWidth()
+	f3=f2[i]
+	f3:SetSize(s,s)
+	f3:ClearAllPoints()
 	if i>6 then
 		f3:SetPoint("BOTTOMRIGHT",f2[i-3],tr,0,0)
 	else
@@ -12,10 +18,10 @@ function
 end
 
 function
-	CBF(f,i) bf=CreateFrame("Button",f:GetName().."Debuff"..i,f,"CompactDebuffTemplate");
-	bf.baseSize=options.iconSize;
+	CBF(f,i) bf=CreateFrame("Button",f:GetName().."Debuff"..i,f,"CompactDebuffTemplate")
+	bf.baseSize=ICON_SIZE
 	bf:SetSize(f.buffFrames[1]:GetSize())
-end;
+end
 
 function
 	mv(f)
@@ -25,17 +31,17 @@ function
 end
 
 function
-	mv3(f) CompactUnitFrame_SetMaxDebuffs(f,12);
+	mv3(f) CompactUnitFrame_SetMaxDebuffs(f,12)
 	if not f.debuffFrames[4] then
 		for i=4,12 do
 			CBF(f,i)
 		end
 	end
 	mv(f)
-end;
+end
 
 hooksecurefunc("CompactUnitFrame_UpdateDebuffs",function(f)
 	if f:GetName():match("^Compact") then
 		mv3(f)
 	end
-end);
+end)
